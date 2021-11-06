ac99fed35ddf8b1db25a761fb18b55c24da6d0bd
tmdoi <doi@osaka-pct.ac.jp>
2021-11-07 05:40:08 +0900

    BulletCreate

---
+++ VRShooting/Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab; // 弾のプレハブ
+    [SerializeField] Transform gunBarrelEnd;  // 銃口(弾の発射位置)
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 入力に応じて弾を発射する
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // プレハブを元に、シーン上に弾を生成
+        Instantiate(bulletPrefab, gunBarrelEnd.position, gunBarrelEnd.rotation);
+    }
+}
